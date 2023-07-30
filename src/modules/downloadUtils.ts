09c73b286e5bd80b88bc183925506b19ceeabd96
Pascal Flores <pascal.flores@etu.univ-amu.fr>
2023-07-31 01:34:55 +0200

    added fetchAndRetry

---
+++ src/modules/downloadUtils.ts
@@ -0,0 +1,25 @@
+
+/**
+ * A fetch wrapper that retries the request if it fails for a given number of times
+ * @param url The url to fetch
+ * @param options The fetch options 
+ * @param retries The number of times the request should be retried if it fails
+ * @returns A promise that resolves with the response or rejects if the request fails after all the retries
+ * @see {@link https://markmichon.com/automatic-retries-with-fetch}
+ */
+export async function fetchAndRetry(url : string, options : RequestInit = {}, retries = 3) : Promise<Response> {
+    return fetch(url, options).then((response) => {
+        if (response.ok)
+            return response;
+        else if (retries > 0)
+            return fetchAndRetry(url, options, retries - 1);
+        else
+            throw new Error('Failed to fetch ' + url);
+    })
+    .catch((error) => {
+        if (retries > 0)
+            return fetchAndRetry(url, options, retries - 1);
+        else
+            throw error;
+    });
+}
